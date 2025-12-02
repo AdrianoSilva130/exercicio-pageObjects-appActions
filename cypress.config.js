173722d5df4efe9b2a9c28e61dfed456e8ad12d6
Adriano Silva <adrianosilva130@gmail.com>
2025-12-02 12:42:44 -0300

    primeiro commit

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,12 @@
-module.exports = {
+const { defineConfig } = require("cypress");
+const lens = require("cypress-lens/plugin");
+
+module.exports = defineConfig({
+  video: false,
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      lens(on, config); 
+      return config;
     },
   },
-};
+});
