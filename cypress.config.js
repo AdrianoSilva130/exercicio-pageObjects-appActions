65cfefba4694d7d8dd18c58291379fa351a5aac1
Adriano Silva <adrianosilva130@gmail.com>
2026-02-09 14:59:28 -0300

    html false

--- cypress.config.js
+++ cypress.config.js
@@ -6,7 +6,7 @@ module.exports = defineConfig({
     reportDir: "mochawesome-report",
     overwrite: false,
     reportFilename: "index.html",
-    html: true,
+    html: false,
     json: true
   },
 
